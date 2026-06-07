ef40763b54914f3a602e6559fd1e9411c165ad60
KUSHEEN Dhar <kusheendhar@gmail.com>
2026-06-07 16:42:14 +0530

    url fixes

--- goodness-glamour/goodness-glamour-backend/server.js
+++ goodness-glamour/goodness-glamour-backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://goodness-glamour-4u7x.onrender.com"
+    "https://goodness-glamour-2.onrender.com"
   ],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
